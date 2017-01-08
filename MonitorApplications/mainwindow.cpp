3ec1f9b790005bf98d3bbf407f1025b7b658d13e
AllForOwl <sahavrysyuk@gmail.com>
2017-01-08 21:00:08 +0400

    Add function for read process

--- MonitorApplications/mainwindow.cpp
+++ MonitorApplications/mainwindow.cpp
@@ -8,6 +8,45 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void MainWindow::GetProcessList(HANDLE CONST hStdOut)
+{
+    PROCESSENTRY32 peProcessEntry;
+
+    HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(
+                             TH32CS_SNAPPROCESS, 0);
+    if(INVALID_HANDLE_VALUE == hSnapshot)
+    {
+        return;
+    }
+
+    peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+    Process32First(hSnapshot, &peProcessEntry);
+    m_allProcessRunning.clear();
+    do
+    {
+    if (!std::find(m_allProcessRunning.begin(), m_allProcessRunning.end(), peProcessEntry.szExeFile))
+    {
+       m_allProcessRunning.push_back(peProcessEntry.szExeFile);
+    }
+    }
+    while(Process32Next(hSnapshot, &peProcessEntry));
+    CloseHandle(hSnapshot);
+
+
+    emit changeQuentitySecondForHour();
+    
+    emit changeQuentitySecondForDay();
+
+    emit changeQuentitySecondForYear();
+}
+
+INT main()
+{
+    HANDLE CONST hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    PrintProcessList(hStdOut);
+    ExitProcess(0);
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
--- MonitorApplications/mainwindow.h
+++ MonitorApplications/mainwindow.h
@@ -3,9 +3,16 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
+#include <windows.h>
+#include <tlhelp32.h>
+
+namespace Ui
+{
+    class MainWindow;
 }
+
+using std::string;
+using std::vector;
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +22,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private:
+    void GetProcessList(HANDLE CONST hStdOut);
+
+private:// add vector minutes
+    struct Program
+    {
+        string  m_name;
+        int     m_quentitySecond;
+    };
+
+    vector<Program> m_hour;
+    vector<Program> m_month;
+    vector<Program> m_year;
+
+    vector<string>  m_allProcessRunning;
+
+    QTimer* m_timeReset
+
     Ui::MainWindow *ui;
 };
 
